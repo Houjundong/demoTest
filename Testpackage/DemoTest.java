60250a9352a1e7d07f025edf0c95a24a9b5c8982
houjundong <3087403604@qq.com>
2019-04-18 17:54:45 +0800

    Initial commit

---
+++ Testpackage/DemoTest.java
@@ -0,0 +1,20 @@
+package Testpackage;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2019/4/17.
+ */
+public class DemoTest {
+    public  void test02() {
+        System.out.println("HelloWorld");
+        System.out.println(6 / 2);
+    }
+
+    @Test
+    public void test03() {
+        System.out.println("HelloWorld");
+    }
+
+
+}
